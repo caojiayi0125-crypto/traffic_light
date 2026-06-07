7419ef5cf9d87b434c80632df27f9374c36d951e
annicondali <annicondali@gmail.com>
2026-06-07 15:45:08 +0800

    完成了keil工程的同步

--- User/main.c
+++ User/main.c
@@ -1,17 +1,7 @@
 #include "stm32f10x.h"
-#include "Delay.h"
-#include "OLED.h"
-#include "Serial.h"
-#include "LED.h"
-#include "Key.h"
 
 int main(void)
 {
-	OLED_Init();
-	LED_Init();
-	Key_Init();
-	Serial_Init();
-
 	while (1)
 	{
 	}
